45892790cbf28ddd2163aac1a92b57bd8a8d8c7d
ashtilawat23 <ashtilawat23@gmail.com>
2020-12-11 10:44:49 -0500

    Styling added, App complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
+import Image from './Image'
+import Video from "./Video";
+import styled from 'styled-components'
 
 
 function App() {
@@ -20,15 +23,42 @@ useEffect(() => {
 
 }, []);
 
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
+    <StyledApp className="App">
+      <h1>
+        NASA'S PHOTO OF THE DAY <span role="img" aria-label='go!'>🚀</span>!
+      </h1>
+      <p className = 'date'> Today's date: {data.date}</p>
+      <h3>{data.title}</h3>
+      {data.media_type==='image' ? <Image data={data}/> : <Video data={data}/>}
+      <div className = 'description'>
+      <p>Credits: {data.copyright}</p>
+        <p className = 'blurb'>{ data.explanation }
+          </p>
+      </div>
+    </StyledApp>
   );
 }
+
+const StyledApp = styled.div`
+  padding: 3%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: white;
+  margin:-25px 0;
+  .date{
+    text-decoration: underline;
+  }
+  .description {
+    width: 80%;
+    margin: 0 auto;
+  }
+  .description .blurb{
+    font-weight: bold;
+  }
+`
+
 
 export default App;
 
